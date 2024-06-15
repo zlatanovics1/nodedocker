eec426ad37364e056bdeaa00343d098a3d45cf38
zlatanoviccs <zlatanovichs6@gmail.com>
2024-06-15 21:42:38 +0200

    Initialized event emitter

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-class App {
-  #name;
+const EventEmitter = require("events");
+
+class ExtendedEventEmitter extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  emitWithTimestamp(eventName, ...args) {
+    const timestamp = new Date().toISOString();
+    this.emit(eventName, ...args, timestamp);
+  }
+
+  onceWithTimeout(eventName, listener, timeout) {
+    const wrapper = (...args) => {
+      clearTimeout(timer);
+      listener.apply(this, args);
+    };
+
+    const timer = setTimeout(() => {
+      this.removeListener(eventName, wrapper);
+    }, timeout);
+
+    this.once(eventName, wrapper);
+  }
 }
+
+module.exports = ExtendedEventEmitter;
